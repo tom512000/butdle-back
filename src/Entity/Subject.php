94e0801879b9804a838abd2816dcc8319caa39be
tom512000 <tom.sikora03@gmail.com>
2025-01-08 19:17:45 +0100

    feat: api platform on entities

--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SubjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SubjectRepository::class)]
+#[ApiResource]
 class Subject
 {
     #[ORM\Id]
